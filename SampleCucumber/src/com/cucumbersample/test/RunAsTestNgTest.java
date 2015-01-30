0fb150b16578f42d995aa4a209960c58e6d42226
manojrg86 <manojrg86@gmail.com>
2015-01-30 14:56:09 -0600

    Changes for testng test

--- SampleCucumber/src/com/cucumbersample/test/RunAsTestNgTest.java
+++ SampleCucumber/src/com/cucumbersample/test/RunAsTestNgTest.java
@@ -4,13 +4,14 @@ import org.testng.annotations.Test;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.TestNGCucumberRunner;
 
-@CucumberOptions(features = {"src/test/resources/"})
+@CucumberOptions(features = {"src/test/resources/"},tags={"@Search1"})
 public class RunAsTestNgTest{
 
 	/**
 	 * Create one test method that will be invoked by TestNG and invoke the
 	 * Cucumber runner within that method.
 	 */
+	
 	@Test(groups = "examples-testng", description = "Example of using TestNGCucumberRunner to invoke Cucumber")
 	public void runTest() {
 		new TestNGCucumberRunner(getClass()).runCukes();
--- SampleCucumber/src/test/resources/Site.feature
+++ SampleCucumber/src/test/resources/Site.feature
@@ -1,5 +1,5 @@
 Feature: Google Search
-
+@Search1
 Scenario: Advance Search in Google
             Given user is on google search page
             When enter "testing" text into search field
@@ -8,7 +8,7 @@ Scenario: Advance Search in Google
             And click on advance search link
             And click on advance search button
             Then verify first link text "testing"
-
+@Search2
 Scenario: Advance Search in Google for cucumber
             Given user is on google search page
             When enter "cucumber" text into search field
