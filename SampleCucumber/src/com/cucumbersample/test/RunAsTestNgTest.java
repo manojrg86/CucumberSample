4d0da09d6c68d03f8f69f1cfd49ea226c0e2ad21
manojrg86 <manojrg86@gmail.com>
2015-01-30 14:04:13 -0600

    Adding changes for testng

---
+++ SampleCucumber/src/com/cucumbersample/test/RunAsTestNgTest.java
@@ -0,0 +1,19 @@
+package com.cucumbersample.test;
+
+import org.testng.annotations.Test;
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.TestNGCucumberRunner;
+
+@CucumberOptions(features = {"src/test/resources/"})
+public class RunAsTestNgTest{
+
+	/**
+	 * Create one test method that will be invoked by TestNG and invoke the
+	 * Cucumber runner within that method.
+	 */
+	@Test(groups = "examples-testng", description = "Example of using TestNGCucumberRunner to invoke Cucumber")
+	public void runTest() {
+		new TestNGCucumberRunner(getClass()).runCukes();
+	}
+
+}
--- SampleCucumber/src/test/resources/Site.feature
+++ SampleCucumber/src/test/resources/Site.feature
@@ -8,3 +8,12 @@ Scenario: Advance Search in Google
             And click on advance search link
             And click on advance search button
             Then verify first link text "testing"
+
+Scenario: Advance Search in Google for cucumber
+            Given user is on google search page
+            When enter "cucumber" text into search field
+            And click on search button
+            And click on advance search icon
+            And click on advance search link
+            And click on advance search button
+            Then verify first link text "cucumber"
